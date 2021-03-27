b32371dda1a18136e3a3c10330a1cfb3b1c22f75
phamhoc-99 <hoanghoatham1999@gmail.com>
2021-03-28 00:04:39 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="App flex h-screen items-center justify-center">
+<div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+    <div className="mb-4">
+      <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="username">
+        Username
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username" />
     </div>
+    <div className="mb-6">
+      <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
+        Password
+      </label>
+      <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************" />
+      <p className="text-red text-xs italic">Please choose a password.</p>
+    </div>
+    <div className="flex items-center justify-between">
+      <button className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button">
+        Sign In
+      </button>
+      <a className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
+        Forgot Password?
+      </a>
+    </div>
+</div>
+</div> 
   );
 }
-
 export default App;
